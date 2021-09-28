035cf63540fabf34cb644b7a808be582ac847792
leandrov27 <42523346+leandrov27@users.noreply.github.com>
2021-09-28 07:02:35 -0400

    Update mask_detector_app.py

--- mask_detector_app.py
+++ mask_detector_app.py
@@ -1,4 +1,20 @@
+# Import `tensorflow`
+
 import tensorflow as tf
+
+# Initialize two constants
+
+x1 = tf.constant([1,2,3,4])
+
+x2 = tf.constant([5,6,7,8])
+
+# Multiply
+
+result = tf.multiply(x1, x2)
+
+# Print the result
+
+print(result)
 import numpy as np
 from training import config_tr
 import streamlit as st
